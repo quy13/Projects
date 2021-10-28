b25c0ce2da12536932f7edb9d2bf480afd3afbf2
quy13 <87173367+quy13@users.noreply.github.com>
2021-10-28 14:11:28 +0700

    155. Dicee Challenge Step 5 - Change both <img> Elements

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,9 +1,9 @@
-var randomNumber1;
-randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
-randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var player1Dice = "images/dice"+randomNumber1+".png" ;
-var player2Dice = "images/dice"+randomNumber1+".png" ;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var player1Dice = "images/dice" + randomNumber1 + ".png" ;
+var player2Dice = "images/dice" + randomNumber2 + ".png" ;
 
 document.querySelector(".img1").setAttribute("src", player1Dice);
 document.querySelector(".img2").setAttribute("src", player2Dice);
