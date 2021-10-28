d5e04eee5184132133268d8030afd085839908f9
quy13 <87173367+quy13@users.noreply.github.com>
2021-10-28 17:18:27 +0700

    162. Adding Event Listeners to a Button

---
+++ Drum Kit/index.js
@@ -0,0 +1,10 @@
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+
+
+for (var i = 0; i < numberOfDrumButton; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+      alert("i got clicked!");  
+    });
+
+}
