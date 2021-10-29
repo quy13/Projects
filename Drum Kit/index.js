b67d259edc579993d4b86bb4a06aac8983bf50e2
quy13 <87173367+quy13@users.noreply.github.com>
2021-10-29 13:52:17 +0700

    165. How to Play Sounds on a Website

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,10 +1,12 @@
 var numberOfDrumButton = document.querySelectorAll(".drum").length;
+var x = "green";
 
 
 for (var i = 0; i < numberOfDrumButton; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-      alert("i got clicked!");  
-    });
+  this.style.color = "white";
+  });}
 
-}
+  // var audio = new Audio('sounds/tom-1.mp3');
+  // audio.play();
