a32c917ea5ee3da0eca944bfa6ede39b81899279
quy13 <87173367+quy13@users.noreply.github.com>
2021-11-01 17:18:38 +0700

    177-184

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,28 @@
-$("h1").css("color", "red");
+$("h1").addClass("big-title margin-50");
+$("a").attr("href", "https://www.yahoo.com");
+// var x = 0;
+// var colors;
+// $("button").click(function() {
+//   if (x % 2 === 0) {
+//     colors = "purple";
+//   } else {
+//     colors = "yellow";
+//   }
+//   x++;
+//   console.log(x);
+//   $("h1").css("color", colors);
+//
+// });
+
+$(document).keypress(function(event) {
+  // console.log(event.key);
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "green");
+});
+
+$("button").on("click", function() {
+  $("h1").fadeToggle();
+});
