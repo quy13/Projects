669977706b256deb86f235667ce5dd5b0d7f0be9
quy13 <87173367+quy13@users.noreply.github.com>
2021-11-02 15:22:29 +0700

    197. Step 5 - Add Sounds to Button Clicks

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -13,27 +13,35 @@ function nextSequence() {
   //put the color into gamePattern array
   gamePattern.push(randomChosenColor);
 
+  playSound(randomChosenColor);
+
+ return randomChosenColor;
+}
+
+//sounds
+function playSound(name){
   // animations
-  $("#"+randomChosenColor).fadeOut(100).fadeIn(100)
+  $("#"+name).fadeOut(100).fadeIn(100)
   // will play sound corresponding to the color
-  var audio = new Audio("sounds/"+randomChosenColor+".mp3");
+  var audio = new Audio("sounds/"+name+".mp3");
   audio.play();
 }
 
 //detect keyboard press
-$(document).on("keypress",function() {
+$(document).on("keypress",function(event) {
 
   nextSequence();
-
+  
 });
+
 //detect moouse click
 $(".btn").on("click",function() {
 
   // get the color of the button that user click
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
   console.log(userClickedPattern);
-
 });
 
 nextSequence();
