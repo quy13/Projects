b53ee37b221bb05fabbef5e050c0e13e41e5f0a3
quy13 <87173367+quy13@users.noreply.github.com>
2021-11-03 10:46:28 +0700

    203. Step 8 - Check the User's Answer Against the Game Sequence8

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -7,6 +7,10 @@ var started = false;
 var level = 0;
 
 function nextSequence() {
+
+  //make a new userClickedPattern array every time nextSequence run
+  userClickedPattern = [];
+
   //generate a random int number from 0-3 range
   var randomNumber = Math.floor(Math.random() * 4);
   //will get random color from the buttonColors array
@@ -14,51 +18,90 @@ function nextSequence() {
   //put the color into gamePattern array
   gamePattern.push(randomChosenColor);
   // animations
-  $("#"+randomChosenColor).fadeOut(100).fadeIn(100)
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
 
   playSound(randomChosenColor);
 
   // increase the level by 1 every time nextSequence() is called.
   level++;
   // update the h1 with change in the value of level.
-  $("#level-title").text("Level "+level);
+  $("#level-title").text("Level " + level);
+
+}
+
+function checkAnswer(lastColor) {
+
+  // logging user & game array
+  console.log(`userTotal [${userClickedPattern.length - 1}] ${userClickedPattern}`);
+  console.log(`gameTotal [${gamePattern.length - 1}] ${gamePattern}`);
+
+  // check user answer with the game
+  if (userClickedPattern[lastColor] === gamePattern[lastColor]) {
+
+    // check if the user finish their answer sequence
+    if (userClickedPattern.length === gamePattern.length) {
+      console.log("Correct!");
+
+      //will call nextSequence after 1 second : 1000 millisecond delay
+      setTimeout(function() {
+        nextSequence();
+      }, 1000)
+    }
+
+  } else {
+
+    console.log("Incorrect!");
+    // reset userClickedPattern array if wrong
+    userClickedPattern = [];
+  }
 
 }
 
 //sounds
-function playSound(name){
+function playSound(name) {
   // will play sound corresponding to the color
-  var audio = new Audio("sounds/"+name+".mp3");
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
 //click animation
-function animatePress(currentColor){
-  $("#"+currentColor).addClass("pressed");
-  setTimeout(function(){
-    $("#"+currentColor).removeClass("pressed");
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
 
-//detect keyboard press - begin the games
-$(document).on("keypress",function(event) {
+//keyboard press - begin the games
+$(document).on("keypress", function() {
 
-  // only activate onces
- if(!started){
-   nextSequence();
-   started = true;
- }
+  // only activate once the game start
+  if (!started) {
+
+    // The h1 title starts out saying "Press A Key to Start",
+    // when the game has started, change this to say "Level 0"
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 
 });
 
-//detect mouse click
-$(".btn").on("click",function() {
+//mouse click
+$(".btn").on("click", function() {
 
   // get the color of the button that user click
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
+
   playSound(userChosenColor);
   animatePress(userChosenColor)
-  console.log(userClickedPattern);
+  // console.log(userClickedPattern);
+
+  // Call checkAnswer() after a user has clicked and chosen their answer,
+  // passing in the index of the last answer in the user's sequence.
+  // make the length begin at 0(start of array)
+  checkAnswer(userClickedPattern.length - 1);
+
 
 });
